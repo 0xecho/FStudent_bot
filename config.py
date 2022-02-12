3d43ff4a3b59bce93fa2af8880e0ac4a0ebc0179
0xecho <0xecho@proto-code.com>
2022-02-12 13:58:24 +0300

    Moves config to environment

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from peewee import *
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 
 db = SqliteDatabase('db.sqlite')
-heartbeat_server = 'http://localhost:5000'
-bot_id = '@fstudent_bot'
+heartbeat_server = os.environ.get("HEARTBEAT_SERVER")
+bot_id = os.environ.get("BOT_ID")
